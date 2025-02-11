05a1578a4939804db95e8839d1a17d79e7b9d93c
leo-braida <leobraidan@gmail.com>
2025-02-11 14:52:03 -0300

    fix: remoção do base_url4 no axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,4 @@
 export const BASE_URL = 'https://my-json-server.typicode.com/leo-braida/jsonfake';
 export const BASE_URL2 = 'https://my-json-server.typicode.com/leo-braida/jsonfake2';
 export const BASE_URL3 = 'https://my-json-server.typicode.com/leo-braida/jsonfake3';
-export const BASE_URL4 = 'https://my-json-server.typicode.com/leo-braida/jsonfake4';
 
